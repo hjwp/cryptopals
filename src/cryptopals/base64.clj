e58e08fefe11f753ba7739a92b7c5bb0042da48f
Harry <hjwp2@cantab.net>
2015-01-04 13:18:25 +0000

    remove dependency on hexstring->int and crazy biginteger calculations

--- src/cryptopals/base64.clj
+++ src/cryptopals/base64.clj
@@ -5,26 +5,41 @@
 (def a-offset (int \a))
 (def zero-offset (int \0))
 
+(defn all [things predicate]
+  (= (count (filter predicate things)) (count things)))
+
+(defn valid-hexchar? [character]
+  (re-matches #"[\da-f]+" (str character)))
+
 (defn valid-hexstring? [string]
-  (re-matches #"[\da-f]+" string))
+  (all string valid-hexchar?))
 
-(defn power [x n]
-  (reduce * (repeat n x)))
+(defn zero-pad [binary-digits desired-num]
+  (let [pad (- desired-num (mod (count binary-digits) desired-num))]
+    (if (= pad desired-num)
+      binary-digits
+      (concat (repeat pad 0) binary-digits))))
 
-(defn hex->int [hex-string]
-  (if (valid-hexstring? hex-string)
-    (read-string (str "0x" hex-string))))
+
+(defn hexchar->int [character]
+  (if (valid-hexchar? character)
+    (read-string (str "0x" (str character)))))
 
 (defn int->bin [number]
-  (if (< number 2) [number]
-    (concat (int->bin (biginteger (/ number 2))) [(mod number 2)])))
+  (cond
+   (< number 2) [number]
+   (> number 15) nil
+   :else (concat (int->bin (int (/ number 2))) [(mod number 2)])))
+
+(defn hexchar->bin [character]
+  (int->bin (hexchar->int character)))
 
 (defn hex->bin [hex-string]
-  (if (re-matches #"[\da-f]+"  hex-string)
-    (int->bin (hex->int hex-string))))
+  (if (valid-hexstring? hex-string)
+    (reduce concat (map #(zero-pad % 4) (map hexchar->bin hex-string)))))
 
 (defn bin->int [binary-digits]
-  (read-string (str "2r" (clojure.string/join binary-digits))))
+  (read-string (str "2r" (string/join binary-digits))))
 
 
 (defn base64char [number]
@@ -35,10 +50,6 @@
    (= number 62) "+"
    (= number 63) "/"))
 
-(defn zero-pad [binary-digits]
-  (let [pad (- 6 (mod (count binary-digits) 6))]
-    (concat (repeat pad 0) binary-digits)))
-
 
 (defn base64 [hex-string]
-  (string/join (map base64char (map bin->int (partition 6 (zero-pad (hex->bin hex-string)))))))
+  (string/join (map base64char (map bin->int (partition 6 (zero-pad (hex->bin hex-string) 6))))))
--- test/cryptopals/base64_test.clj
+++ test/cryptopals/base64_test.clj
@@ -3,25 +3,43 @@
             [cryptopals.base64 :refer :all]))
 
 
-;; hex->int helper function to convert hex strings to integers
-(expect 1 (hex->int "1"))
-(expect 10 (hex->int "a"))
-(expect 16 (hex->int "10"))
-(expect 26 (hex->int "1a"))
-(expect nil (hex->int "z"))
-; check int->bin helper function doesn't blow the stack
-(expect [1 0 0 0 0 0 0 0 0 0 0 0 0 0] (int->bin 8192))
+;; hexchar->int helper function to convert hex characters to integers
+(expect 1 (hexchar->int \1))
+(expect 2 (hexchar->int \2))
+(expect 10 (hexchar->int \a))
+(expect 15 (hexchar->int \f))
+(expect nil (hexchar->int \g))
+
+; int->bin helper should allow numbers greater than a single hex char repr
+(expect nil (int->bin  16))
+
+
+(expect [0 0 1] (zero-pad [1] 3))
+(expect [0 0 1] (zero-pad [0 0 1] 3))
+(expect [1 0 1] (zero-pad [1 0 1] 3))
+(expect [0 0 0 0 0 1] (zero-pad [1] 6))
+(expect [0 0 0 0 0 1 0 1 0 1 0 1] (zero-pad [1 0 1 0 1 0 1] 6))
 
 
 ;; hex->bin helper function to convert hex strings to binary
-(expect [0] (hex->bin "0"))
-(expect [1] (hex->bin "1"))
-(expect [1 0] (hex->bin "2"))
-(expect [1 1] (hex->bin "3"))
-(expect [1 0 0] (hex->bin "4"))
+(expect [0] (hexchar->bin \0))
+(expect [1] (hexchar->bin \1))
+(expect [1 0] (hexchar->bin \2))
+(expect [1 1] (hexchar->bin \3))
+(expect [1 0 0] (hexchar->bin \4))
+(expect [1 0 0 0] (hexchar->bin \8))
+(expect [1 0 1 0] (hexchar->bin \a))
+(expect [1 1 1 1] (hexchar->bin \f))
+
+(expect [0 0 0 0] (hex->bin "0"))
+(expect [0 0 0 1] (hex->bin "1"))
+(expect [0 0 1 0] (hex->bin "2"))
+(expect [0 0 1 1] (hex->bin "3"))
+(expect [0 1 0 0] (hex->bin "4"))
 (expect [1 0 0 0] (hex->bin "8"))
-(expect [1 0 0 0 0] (hex->bin "10"))
 (expect [1 0 1 0] (hex->bin "a"))
+(expect [0 0 0 1 0 0 0 0] (hex->bin "10"))
+(expect [0 0 0 1 1 1 1 1] (hex->bin "1f"))
 (expect nil (hex->bin "z"))
 
 ;; convert binary sequence back to ints
@@ -29,6 +47,7 @@
 (expect 1 (bin->int [1]))
 (expect 2 (bin->int [1 0]))
 (expect 3 (bin->int [1 1]))
+
 
 ;; base64 char of an integer
 (expect "A" (base64char 0))
@@ -40,8 +59,6 @@
 (expect "+" (base64char 62))
 (expect "/" (base64char 63))
 
-(expect [0 0 0 0 0 1] (zero-pad [1]))
-(expect [0 0 0 0 0 1 0 1 0 1 0 1] (zero-pad [1 0 1 0 1 0 1]))
 
 
 ;; and finally, our base64 function
