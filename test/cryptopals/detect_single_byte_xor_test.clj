cae9d553ed33556e8fb3774a9af96c35c0ad8e2a
Harry <hjwp2@cantab.net>
2015-01-16 19:27:10 +0000

    try manually applying most-likely-decrypt, doesnt work

--- test/cryptopals/detect_single_byte_xor_test.clj
+++ test/cryptopals/detect_single_byte_xor_test.clj
@@ -1,11 +1,18 @@
 (ns cryptopals.detect-single-byte-xor-test
   (:require [expectations :refer [expect]]
-            [cryptopals.detect-single-byte-xor :refer :all]
+            [cryptopals.single-byte-xor-cypher :refer :all]
+            [clojure.string :as string]
             [cryptopals.detect-single-byte-xor :refer :all]))
 
 ; Detect single-character XOR
 ; One of the 60-character strings in detect-single-byte-xor.txt has been encrypted by single-character XOR.
 ; Find it.
 
-(def encrypted-strings (slurp "resources/detect-single-byte-xor.txt"))
+(def encrypted-strings-file (slurp "resources/detect-single-byte-xor.txt"))
+(def encrypted-strings (string/split encrypted-strings-file #"\n"))
+encrypted-strings
 
+
+;; (def decrypts (map most-likely-single-byte-xor-decrypt encrypted-strings))
+;; decrypts
+;; (last (sort-by :score decrypts))
